05f04ad0c67da06a603003f77ca2984be120794c
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-13 13:08:55 -0500

    FirstClassMade

---
+++ src/outputExamples/FirstClassMade.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class FirstClassMade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Koomeng");
+		System.out.println("Xiong");
+	}
+
+}
