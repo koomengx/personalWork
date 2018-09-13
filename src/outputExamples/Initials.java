642545dfe17b3b10e03633f38dcebe88469e0055
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-13 14:08:06 -0500

    Initials

--- src/outputExamples/Initials.java
+++ src/outputExamples/Initials.java
@@ -4,13 +4,13 @@ public class Initials {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("k    k x      x");
-		System.out.println("k  k    x    x");
-		System.out.println("k k      x  x");
-		System.out.println("kk        x");
-		System.out.println("k k     x   x");
-		System.out.println("k  k   x     x");
-		System.out.println("k    kx       x");
+		System.out.println("k    kx      x");
+		System.out.println("k  k   x    x");
+		System.out.println("k k     x  x");
+		System.out.println("kk       x");
+		System.out.println("k k     x  x");
+		System.out.println("k  k   x    x");
+		System.out.println("k    kx      x");
 	}
 
 }
