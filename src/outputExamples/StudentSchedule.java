b68b81381cb47e1466b43513fcb93eaca3a72de6
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-13 13:40:01 -0500

    Student Schedule

---
+++ src/outputExamples/StudentSchedule.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class StudentSchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Koomeng Xiong");
+		System.out.println("1st Block: Study Hall");
+		System.out.println("2nd Block: Ap Psych");
+		System.out.println("3rd Block: Ap AB Calculus");
+		System.out.println("4th Block Ap Computer Science");
+	}
+
+}
