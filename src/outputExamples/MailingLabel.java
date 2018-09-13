9540368dd62d19a77c38b2872ea889b629939363
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-13 13:50:46 -0500

    Mailing Label

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Koomeng Xiong \n 7829 W Herbert Ave \n Milwaukee WI 53218");
+	}
+
+}
