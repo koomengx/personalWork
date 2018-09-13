6478bd4aca69c96b45f45b9e76ee98546f475d6d
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-13 13:16:34 -0500

    FirstLastAge

---
+++ src/outputExamples/FirstLastAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstLastAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Koomeng");
+		System.out.println("Xiong");
+		System.out.println("16");
+	}
+
+}
