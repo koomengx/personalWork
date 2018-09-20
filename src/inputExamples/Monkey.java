7b76c0d51c73250249d6142e052afba8b8176940
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-20 13:09:50 -0500

    inputExamples

---
+++ src/inputExamples/Monkey.java
@@ -0,0 +1,14 @@
+package inputExamples;
+
+import java.util.Scanner;
+public class Monkey{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+double height = -999.0;
+Scanner input = new Scanner(System.in);
+System.out.println("How tall is an adult monkey" );
+height = input.nextDouble();
+System.out.println("Adult Monkey =" height + "pounds");
+	}
+
+}
