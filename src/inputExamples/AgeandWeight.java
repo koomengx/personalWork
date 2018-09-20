99c15874144101a99622da88b110961e27ebfe8e
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-20 13:55:16 -0500

    ctrlshiftf

--- src/inputExamples/AgeandWeight.java
+++ src/inputExamples/AgeandWeight.java
@@ -1,19 +1,20 @@
 package inputExamples;
 
 import java.util.Scanner;
+
 public class AgeandWeight {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int age = -999;
-int weight = -999;
-Scanner input = new Scanner(System.in);
-System.out.println("How old are you?");
-age = input.nextInt();
-System.out.println("You are " + age + " Years Old");
-System.out.println("What is your weight?");
-weight = input.nextInt();
-System.out.println("You are " + weight + " lbs");
+		int age = -999;
+		int weight = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("How old are you?");
+		age = input.nextInt();
+		System.out.println("You are " + age + " Years Old");
+		System.out.println("What is your weight?");
+		weight = input.nextInt();
+		System.out.println("You are " + weight + " lbs");
 	}
 
 }
---
+++ src/inputExamples/LockerCombo.java
@@ -0,0 +1,11 @@
+package inputExamples;
+
+import 
+public class LockerCombo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
