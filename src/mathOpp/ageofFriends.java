261488eb05c0f996a3dc98c78d38c0a6a6bad0fa
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-21 14:49:36 -0500

    AgeofFriends

---
+++ src/mathOpp/ageofFriends.java
@@ -0,0 +1,21 @@
+package mathOpp;
+
+import java.util.Scanner;
+public class ageofFriends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int sum = -999;
+int friend1 = -999;
+int friend2 = -999;
+int friend3 = -999;
+Scanner input = new Scanner(System.in);
+System.out.println("What are the ages of your 3 friends?");
+friend1 = input.nextInt();
+friend2 = input.nextInt();
+friend3 = input.nextInt();
+sum = friend1 + friend2 + friend3;
+System.out.println("The sum of your 3 friends ages is " + sum);
+	}
+
+}
