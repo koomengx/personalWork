0b24840f6d6d5396b5a547f2d0cfcc6614d03945
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-09-21 14:42:07 -0500

    fixing

--- src/mathOpp/Age.java
+++ src/mathOpp/Age.java
@@ -1,16 +1,17 @@
 package mathOpp;
 
 import java.util.Scanner;
+
 public class Age {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int age = -999;
-Scanner input = new Scanner(System.in);
-System.out.println("what is your age?");
-age = input.nextInt();
-age = age + 7;
-System.out.println("Seven years from now you will be " + age + " years old");
+		int age = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("what is your age?");
+		age = input.nextInt();
+		age = age + 7;
+		System.out.println("Seven years from now you will be " + age + " years old");
 
 	}
 
--- src/mathOpp/YearstoVote.java
+++ src/mathOpp/YearstoVote.java
@@ -1,15 +1,17 @@
 package mathOpp;
+
 import java.util.Scanner;
+
 public class YearstoVote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int age = -999;
-Scanner input = new Scanner(System.in);
-System.out.println("How old are you?");
-age = input.nextInt();
-age = 18 - age;
-System.out.println("You have " + age + " years until you can vote");
+		int age = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("How old are you?");
+		age = input.nextInt();
+		age = 18 - age;
+		System.out.println("You have " + age + " years until you can vote");
 	}
 
 }
