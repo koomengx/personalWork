fb768355b88add17e2fd552435fd0cbef9659ba6
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-10-03 14:36:44 -0500

    Room

---
+++ src/OOP/RoomTestClass.java
@@ -0,0 +1,12 @@
+package OOP;
+import java.util.Scanner;
+public class RoomTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Room area = new Room();
+area.sayArea();
+area.sayAreaWithDoor();
+	}
+
+}
