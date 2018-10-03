143617a1d5bb4aa7e78bda8863bee5600ea90e33
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-10-03 15:01:14 -0500

    Room2

--- src/OOP/RoomTestClass.java
+++ src/OOP/RoomTestClass.java
@@ -4,9 +4,12 @@ public class RoomTestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Room area = new Room();
-area.sayArea();
-area.sayAreaWithDoor();
+Room closet = new Room();
+Room kitchen = new Room();
+kitchen = closet;
+Room basement = new Room();
+closet = basement;
+closet.sayArea();
 	}
 
 }
