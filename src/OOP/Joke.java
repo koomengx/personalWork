fe8ab3f3d19e2f2f33b9f6914ffbadfb3ca2e850
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-10-03 14:08:19 -0500

    Joke

---
+++ src/OOP/Joke.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class Joke {
+public Joke()
+{
+}
+public void sayJoke()
+{
+	System.out.println("Food");
+}
+public void sayMoney()
+{
+	System.out.println("I want my money!");
+}
+public void sayConversation()
+{
+	System.out.println("Hello");
+}
+}
---
+++ src/OOP/JokeTestClass.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class JokeTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Joke gene = new Joke();
+gene.sayConversation();
+gene.sayJoke();
+gene.sayMoney();
+	}
+
+}
