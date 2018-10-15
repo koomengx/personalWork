321051be5d736a33993786c3265dabbdc8285279
Things <koomeng.xiong21@gmail.com>
2018-10-15 14:45:38 -0500

    Right Triangle

---
+++ src/OOP/RightTriangleClass.java
@@ -0,0 +1,18 @@
+package OOP;
+
+public class RightTriangleClass {
+	private double base;
+	private double height;
+	public double getBase(){
+		return base;
+	}
+	public double getHeight(){
+		return height;
+	}
+	public void setBase(double xBase){
+		
+	}
+	public void setHeight(double xHeight){
+		
+	}
+}
