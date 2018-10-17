693fb21305159aed01d55017ae5fbab97cf478c6
Things <koomeng.xiong21@gmail.com>
2018-10-17 14:54:34 -0500

    Decisions Day 2

---
+++ src/Decisions/age.java
@@ -0,0 +1,68 @@
+package Decisions;
+
+import java.util.Scanner;
+
+public class age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		/*
+		 * System.out.println("What is your Age"); int age = 999; age =
+		 * input.nextInt(); // Compare age
+		 * System.out.println(" what is your height (in inches) and shoe size?"
+		 * ); double height = 999.9; double shoe = 999.9; height =
+		 * input.nextDouble(); shoe = input.nextDouble();
+		 * System.out.println(shoe > 9); System.out.println(height < 62); //
+		 * Height and shoe size int pets = 999; int cars = 999;
+		 * System.out.println("How many pets do you have?"); pets =
+		 * input.nextInt();
+		 * System.out.println("How many cars will fit in your garage?"); cars =
+		 * input.nextInt(); System.out.println(pets <= 5|| cars >3); // Pets and
+		 * Cars int number = 15;
+		 * 
+		 * if(number <= 14){ System.out.println("Nice Job"); }
+		 * System.out.println("What a Program!");
+		 */
+		/*
+		 * double total = 25; char favLetter = 'Q'; if(favLetter == 'X'){
+		 * System.out.println("Great job"); }
+		 * System.out.println("What a program");
+		 */
+		/*
+		  int score1 = 999; int score2 = 999;
+		 System.out.println("What score did you get on the 1st test?"); 
+		 score1 = input.nextInt();
+		  System.out.println("What score did you get on the 2nd test?"); 
+		  score2 = input.nextInt(); 
+		  if(score1 > score2){ System.out.println("Great job for entering the score in the right order"); 
+		  System.out.println("Have a nice da");
+		  }
+		 */
+		/*
+		int one = 1;
+		int two = 2;
+		int three = 3;
+		System.out.println("List three numbers");
+		one = input.nextInt();
+		two = input.nextInt();
+		three = input.nextInt();
+		if(one > two && one  > three){
+			System.out.println("The first number is the largest");
+		}
+		if(two > one && two  > three){
+			System.out.println("The second number is the largest");
+		}
+		if(three > one && three  > two){
+			System.out.println("The third number is the largest");
+		}
+		if(one == two || two ==three || one == three){
+		System.out.println("All numbers are equal");
+		} 
+		THREE NUMBERS
+		*/
+		
+		
+	}
+
+}
