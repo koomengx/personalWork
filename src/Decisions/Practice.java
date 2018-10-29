8a9cbacbc393ce45597fedac67fc0127929269bf
Things <koomeng.xiong21@gmail.com>
2018-10-29 14:50:25 -0500

    Nested Decsions

--- src/Decisions/Practice.java
+++ src/Decisions/Practice.java
@@ -7,12 +7,15 @@ public class Practice {
 		Scanner input  = new Scanner(System.in);
 		int counter = 0;
 		int num = 0;
-		System.out.println("What is your favorite number?");
-		num = input.nextInt();
-		for (counter = 1; counter <= 10; counter++) {
-			System.out.println(num + " Hello");
+		int sum = 0;
+		//System.out.println("What is your favorite number?");
+		//num = input.nextInt();
+		for (counter = 1; counter <= 5; counter++) {
+			System.out.println("Pick a number");
+			num = input.nextInt();
+			sum = sum + num;
 		}
-		
+		System.out.println("The sum of the numbers entered is: " + sum);
 	}
 
 }
