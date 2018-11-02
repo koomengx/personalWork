237aeafcdd6a97924d10d0f1d02978d87f0c442d
Things <koomeng.xiong21@gmail.com>
2018-11-02 14:49:12 -0500

    Loops 2

--- src/Loops/CountBy5.java
+++ src/Loops/CountBy5.java
@@ -1,10 +1,20 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class CountBy5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int num = 5;
+		int multiple = 0;
+	
+		System.out.println("The multiples of 5 less than 49 is");
+		while ( num <= 49){
+		System.out.println( num);
+		num = num + 5;
+		}
 	}
 
 }
