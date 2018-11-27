5b120b1f3c6090170ca0d3e793a823aa31dea005
Koomeng Xiong <koomeng.xiong21@gmail.com>
2018-11-27 15:00:05 -0600

    Arrays Day 2

--- src/Arrays/ArraysPractice.java
+++ src/Arrays/ArraysPractice.java
@@ -9,22 +9,22 @@ public class ArraysPractice {
 		Scanner input = new Scanner(System.in);
 		/*
 		int[] Test = new int[5];
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < Test.length; i++) {
 			System.out.println("Test Score");
 			Test[i] = input.nextInt();
 		}
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < Test.length; i++) {
 			System.out.println(Test[i]);
 		}
 		*/
 		/*
 		int[] Test = new int[5];
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < Test.length; i++) {
 			System.out.println("Test Score");
 			Test[i] = input.nextInt();
 			
 		}
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < Test.length; i++) {
 			System.out.println(Test[i]);
 			int sum += Test[i];
 		}
@@ -40,6 +40,7 @@ public class ArraysPractice {
 			System.out.println(Test[i]);
 		}
 		*/
+		/*
 		Random generator = new Random();
 		int[] scores = new int[250];
 		int min = 0;
@@ -48,6 +49,46 @@ public class ArraysPractice {
 		for(int i=0; i<250; i++){
 			scores[i] = min + generator.nextInt(max - min + 1);
 		}
-	}
+		*/
+		/*
+		int numNames = 20;
+		String[] names = new String[numNames];
+		for(int i = 0; i < names.length; i++){
+			System.out.println("Name:");
+			names[i] = input.nextLine();
+		}
+		for(int a = names.length - 1; a >= 0; a--){
+			System.out.println(names[a]);
+		}
+		*/
+		/*
+		int numNames = 20;
+		String[] names = new String[numNames];
+		for(int i = 0; i < names.length; i++){
+			System.out.println("Name:");
+			names[i] = input.nextLine();
+		}
+		System.out.println("What letter to you want to look up?");
+		String letter = input.nextLine();
+		for(int a = 0; a < names.length; a++){
+			if(names[a].endsWith(letter.substring(0, 1))){
+			System.out.println("The names: " + names[a] +" ends with " + letter);
+			}
+			*/
+		int numNames = 5;
+		String[] names = new String[numNames];
+		for(int i = 0; i < names.length; i++){
+			System.out.println("Name:");
+			names[i] = input.nextLine();
+		}
+		System.out.println("Select a desired character length for the names");
+		int length = input.nextInt();
+		int match = 0;
+		for (int a = 0;a < names.length; a++){
+			if(names[a].length() == length){
+				match++;
+			}
+		}
+	System.out.println( match + " of your friend names matched the length!");
 
-}
+}}
